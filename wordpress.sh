abb1842215d3d83cc8dae615c8b7a8633e16c267
davask <davask.42@gmail.com>
2016-04-12 10:24:01 +0200

    Move common process to d-base

--- wordpress.sh
+++ wordpress.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-service sendmail start
-echo "Sendmail Initialized";
-
-ln -s ${DWL_USER_DIR}/${DWL_APP_DIR} /var/www/html
-
-chown :www-data -R /var/www/html
-find /var/www/html -type d -exec chmod 775 {} \;
-find /var/www/html -type f -exec chmod 664 {} \;
-
 echo "Wordpress Initialized";
