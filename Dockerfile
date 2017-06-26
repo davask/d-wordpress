8e935f166931d4e7e96c4b873bbc6fd39dd4fbfa
davask <contact@davaskweblimited.com>
2017-06-26 08:20:38 +0200

    Automatic update 4.4.2-p7.0-a2.4-d8.8

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,6 @@ USER root
 LABEL dwl.app.cms="WordPress"
 
 
-RUN wget https://wordpress.org/wordpress-4.4.2.tar.gz -O /dwl/default/var/www/wordpress-4.4.2.tar.gz
-RUN rm -rdf /dwl/default/var/www/html
-RUN tar -xzvf /dwl/default/var/www/wordpress-4.4.2.tar.gz -C /dwl/default/var/www
-RUN rm /dwl/default/var/www/wordpress-4.4.2.tar.gz
-RUN mv /dwl/default/var/www/wordpress /dwl/default/var/www/html
-
 # Copy instantiation specific file
 COPY ./build/dwl/get-wordpress.sh /dwl/get-wordpress.sh
 COPY ./build/dwl/fix-wordpress-permissions.sh /dwl/fix-wordpress-permissions.sh
--- build/dwl/get-wordpress.sh
+++ build/dwl/get-wordpress.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 if [ `find /home/${DWLC_USER_NAME}/files -mindepth 1 -maxdepth 1 | wc -l` == "0" ]; then
-    sudo rsync -a /dwl/default/var/www/html/ /home/${DWLC_USER_NAME}/files;
-    sudo mv /home/${DWLC_USER_NAME}/files/wp-config-sample.php /home/${DWLC_USER_NAME}/files/wp-config.php;
+    wget https://wordpress.org/wordpress-${WORDPRESS_VERSION:-4.4.2}.tar.gz -O /tmp/wordpress-${WORDPRESS_VERSION:-4.4.2}.tar.gz;
+    tar -xzvf /tmp/wordpress-${WORDPRESS_VERSION:-4.4.2}.tar.gz -C /home/${DWLC_USER_NAME}/files;
+    mv /home/${DWLC_USER_NAME}/files/wp-config-sample.php /home/${DWLC_USER_NAME}/files/wp-config.php;
+    rm /tmp/wordpress-${WORDPRESS_VERSION:-4.4.2}.tar.gz;
 fi
