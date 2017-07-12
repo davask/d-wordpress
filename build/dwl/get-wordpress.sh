f4b0838a2d3d11af42c11e537f17151818266703
davask <contact@davaskweblimited.com>
2017-07-12 02:42:20 +0200

    Automatic update 4.4.2-p5.6-a2.4-d8.8

--- build/dwl/get-wordpress.sh
+++ build/dwl/get-wordpress.sh
@@ -1,5 +1,6 @@
-#! /bin/bash
-if [ `find /home/${DWLC_USER_NAME}/files -mindepth 1 -maxdepth 1 | wc -l` == "0" ]; then
+#!/bin/bash
+
+if [ `sudo find /home/${DWLC_USER_NAME}/files -mindepth 1 -maxdepth 1 | wc -l` == "0" ]; then
     wget https://wordpress.org/wordpress-${WORDPRESS_VERSION:-4.4.2}.tar.gz -O /tmp/wordpress-${WORDPRESS_VERSION:-4.4.2}.tar.gz;
     tar -xzvf /tmp/wordpress-${WORDPRESS_VERSION:-4.4.2}.tar.gz -C /home/${DWLC_USER_NAME}/files;
     mv /home/${DWLC_USER_NAME}/files/wp-config-sample.php /home/${DWLC_USER_NAME}/files/wp-config.php;
