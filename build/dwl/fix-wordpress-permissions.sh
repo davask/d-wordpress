11bf8a1556342687bbc7a2c48918a89b12e3b257
davask <contact@davaskweblimited.com>
2017-07-13 12:04:44 +0200

    Automatic update 4.4.2-p5.6-a2.4-u16.04

--- build/dwl/fix-wordpress-permissions.sh
+++ build/dwl/fix-wordpress-permissions.sh
@@ -12,19 +12,19 @@ if [ "${DWLC_USER_NAME}" == "" ]; then
 fi
 
 # reset to safe defaults
-find /home/${DWLC_USER_NAME}/files -exec chown ${DWLC_USER_NAME}:${DWLC_USER_NAME} {} \;
-find /home/${DWLC_USER_NAME}/files -type d -exec chmod 775 {} \;
-find /home/${DWLC_USER_NAME}/files -type f -exec chmod 664 {} \;
+sudo find /home/${DWLC_USER_NAME}/files -exec chown ${DWLC_USER_NAME}:${DWLC_USER_NAME} {} \;
+sudo find /home/${DWLC_USER_NAME}/files -type d -exec chmod 775 {} \;
+sudo find /home/${DWLC_USER_NAME}/files -type f -exec chmod 664 {} \;
 
 # allow wordpress to manage wp-config.php (but prevent world access)
 HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`;
 if [ -f /home/${DWLC_USER_NAME}/files/wp-config.php ]; then
-    chgrp ${HTTPDUSER} /home/${DWLC_USER_NAME}/files/wp-config.php
-    chmod 660 /home/${DWLC_USER_NAME}/files/wp-config.php
+    sudo chgrp ${HTTPDUSER} /home/${DWLC_USER_NAME}/files/wp-config.php
+    sudo chmod 660 /home/${DWLC_USER_NAME}/files/wp-config.php
 fi
 # allow wordpress to manage wp-content
 if [ -d /home/${DWLC_USER_NAME}/files/wp-content ]; then
-    find /home/${DWLC_USER_NAME}/files/wp-content -exec chgrp ${HTTPDUSER} {} \;
-    find /home/${DWLC_USER_NAME}/files/wp-content -type d -exec chmod 775 {} \;
-    find /home/${DWLC_USER_NAME}/files/wp-content -type f -exec chmod 664 {} \;
+    sudo find /home/${DWLC_USER_NAME}/files/wp-content -exec chgrp ${HTTPDUSER} {} \;
+    sudo find /home/${DWLC_USER_NAME}/files/wp-content -type d -exec chmod 775 {} \;
+    sudo find /home/${DWLC_USER_NAME}/files/wp-content -type f -exec chmod 664 {} \;
 fi
