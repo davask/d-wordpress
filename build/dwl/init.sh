785898fd80bb58d32b718224711ab65ceec7d41a
davask <contact@davaskweblimited.com>
2017-07-23 17:16:01 +0200

    Automatic update 4.4.2-p5.6-a2.4-u14.04

--- build/dwl/init.sh
+++ build/dwl/init.sh
@@ -4,5 +4,43 @@ cd ~/;
 
 dwlDir="/dwl";
 
+. ${dwlDir}/envvar.sh
+. ${dwlDir}/user.sh
+. ${dwlDir}/ssh.sh
+echo ">> Os initialized";
+
+echo ">> Base initialized";
+
+. ${dwlDir}/permission.sh
+echo ">> Permission assigned";
+
+. ${dwlDir}/activateconf.sh
+echo ">> Dwl conf activated";
+
+. ${dwlDir}/virtualhost.sh
+echo ">> Virtualhost generic update";
+
+. ${dwlDir}/openssl.sh
+echo ">> Openssl initialized";
+
+. ${dwlDir}/certbot.sh
+echo ">> Certbot initialized";
+
+. ${dwlDir}/virtualhost-ssl.sh
+echo ">> SSL initialized";
+
+. ${dwlDir}/virtualhost-tsl.sh
+echo ">> TSL initialized";
+
+. ${dwlDir}/apache2.sh
+echo ">> Apache2 initialized";
+
+. ${dwlDir}/php.sh
+echo ">> Php initialized";
+
+# . ${dwlDir}/get-wordpress.sh
+. ${dwlDir}/fix-wordpress-permissions.sh
+echo ">> Wordpress initialized";
+
 
 tail -f /dev/null;
