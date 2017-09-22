9105f848815fe10c0e5f7d35e42be2cf234a5dcf
davask <contact@davaskweblimited.com>
2017-09-22 05:29:44 +0200

    Automatic update 4.4.2-p5.6-a2.4-u14.04

--- build/dwl/init.sh
+++ build/dwl/init.sh
@@ -43,5 +43,13 @@ echo ">> Php initialized";
 echo ">> Wordpress initialized";
 
 . ${dwlDir}/custom.sh
+echo ">> custom initialized";
+
+# . ${dwlDir}/senmail.sh
+# sendmail is only available from davask/d-php*
+if [ "`dpkg --get-selections | awk '{print $1}' | grep sendmail$ | wc -l`" == "1" ]; then
+  sudo service sendmail start;
+  echo ">> Sendmail initialized";
+fi
 
 tail -f /dev/null;
